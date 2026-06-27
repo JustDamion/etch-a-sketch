ba281ea0070ec2055c66a67c9c8d9518dd96a4db
Damion Shirkey <shirkeydamion@gmail.com>
2026-06-27 13:00:35 -0500

    Create divs for grid

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello world");
+const container = document.querySelector(".container");
+for (let i = 0; i < 32; i++) {
+    const gridSquare = document.createElement("div");
+    container.appendChild(gridSquare);
+}
