47c5441d9a385b7d8f5289d8279a61f8c552dd00
Damion Shirkey <shirkeydamion@gmail.com>
2026-06-27 13:46:34 -0500

    Create base grid

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 32; i++) {
-    const gridSquare = document.createElement("div");
-    container.appendChild(gridSquare);
+for (let i = 0; i < 16; i++) {
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+    container.appendChild(gridRow);
+
+    for (let i = 0; i < 16; i++) {
+        const gridColumn = document.createElement("div");
+        gridColumn.classList.add("grid-column");
+        gridRow.appendChild(gridColumn);
+    }
 }
+
