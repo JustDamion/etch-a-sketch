4f21861f6582447a18427e48c2cfac975831eb75
Damion Shirkey <shirkeydamion@gmail.com>
2026-06-28 11:39:56 -0500

    Change styling

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
+const sizeSlider = document.getElementById("size-slider");
+const sizeLabel = document.getElementById("size-label");
+
 createGrid();
 
-container.addEventListener("mouseover", (event) => {
+grid.addEventListener("mouseover", (event) => {
     const target = event.target;
     target.setAttribute("style", `background-color: ${generateRandomColor()}`);
 });
 
-const newGridButton = document.querySelector("button");
-newGridButton.addEventListener("click", newGrid);
+sizeSlider.addEventListener("change", (event) => {
+    const size = event.target.value;
+    updateGridSize(size);
+    sizeLabel.textContent = `${size} x ${size}`;
+});
 
 function generateRandomColor() {
     const randomHue = Math.random() * 360;
     return `hsla(${randomHue}, 100%, 50%, 1)`;
 }
 
+function clearGrid() {
+    grid.textContent = '';
+}
+
 function createGrid(size = 16) {
-    container.textContent = '';
     for (let i = 0; i < size; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
-        container.appendChild(gridRow);
+        grid.appendChild(gridRow);
 
         for (let i = 0; i < size; i++) {
-            console.log(i);
             const gridColumn = document.createElement("div");
             gridColumn.classList.add("grid-column");
             gridRow.appendChild(gridColumn);
@@ -30,14 +38,8 @@ function createGrid(size = 16) {
     }
 }
 
-function newGrid() {
-    let size = 16;
-    let firstTime = true;
-
-    while (size > 100 || size < 2 || firstTime) {
-        size = firstTime ? +prompt("Enter a grid size") : +prompt("Grid size must be between 2 and 100");
-        firstTime = false;
-    }
-
+function updateGridSize(size) {
+    console.log("Update");
+    clearGrid();
     createGrid(size);
 }
